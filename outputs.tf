30e3e015ee08ae96742a65184f1c88b783650a15
Gustavo Apolinario <gustavo.guss@gmail.com>
2023-11-03 15:44:50 -0300

    uncomment output loadbalancer

--- outputs.tf
+++ outputs.tf
@@ -6,9 +6,9 @@ output "eks_outputs" {
   value = var.all_outputs ? module.eks : null
 }
 
-# output "eks_loadbalancer_outputs" {
-#   value = var.all_outputs ? module.eks_loadbalancer : null
-# }
+output "eks_loadbalancer_outputs" {
+  value = var.all_outputs ? module.eks_loadbalancer : null
+}
 
 output "configure_kubectl" {
   description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
